c7f4971cba87b88af61d4c6b6d0be28db8434b50
Nozimjon <nozimjonjuraev3003@gmail.com>
2022-07-11 21:02:19 +0500

    FEATURE: set configs for deploy the project

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,16 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: "",
 });
